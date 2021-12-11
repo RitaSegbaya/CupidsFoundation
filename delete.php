802c6e4ee17aed58cd83464c482f58ecbd9b2da4
RitaSegbaya <94528508+RitaSegbaya@users.noreply.github.com>
2021-12-11 19:04:09 +0000

    Update delete.php

--- delete.php
+++ delete.php
@@ -6,46 +6,4 @@ $id=$_GET['id'];
 	header('location:admin/admin.php');?>
 <?php
 
-$id=$_GET['projectname'];
 
-	include('admin/connections.php');
-	mysqli_query($con,"delete from `project` where projectname='$id'");
-	header('location:admin/admin.php');?>
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Delete Record</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <style>
-        .wrapper{
-            width: 600px;
-            margin: 0 auto;
-        }
-    </style>
-</head>
-<body>
-    <div class="wrapper">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-12">
-                    <h2 class="mt-5 mb-3">Delete Record</h2>
-                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                        <div class="alert alert-danger">
-                            <input type="hidden" id="id" value="<?php echo trim($_GET["id"]); ?>"/>
-                            <p>Are you sure you want to delete this record?</p>
-                            <p>
-                                <input type="submit" value="Yes" class="btn btn-danger">
-                                <a href="admin/admin.php" class="btn btn-secondary">No</a>
-                            </p>
-                        </div>
-                    </form>
-                </div>
-            </div>        
-        </div>
-    </div>
-</body>
-</html>
