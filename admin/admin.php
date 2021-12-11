8d7cdee4ad779882be63a3e2ea85b9df31d0a6d0
RitaSegbaya <94528508+RitaSegbaya@users.noreply.github.com>
2021-12-11 18:54:12 +0000

    Update admin.php

--- admin/admin.php
+++ admin/admin.php
@@ -174,12 +174,14 @@
                                     echo "<tr>";
                                         echo "<td>" . $row['name'] . "</td>";
                                         echo "<td>" . $row['projectname'] . "</td>";
+                                        
                                                                                 
     
                                         echo "<td>";
-                                            echo '<a href="../read.php?sid='. $row['id'] .'" class="mr-3" title="View Record" data-toggle="tooltip"><span class="fa fa-eye"></span></a>';
-                                            echo '<a href="../insert.php?sid='. $row['id'] .'" class="mr-3" title="Update Record" data-toggle="tooltip"><span class="fa fa-pencil"></span></a>';
-                                            echo '<a href="../delete.php?sid='. $row['id'] .'" title="Delete Record" data-toggle="tooltip"><span class="fa fa-trash"></span></a>';
+        
+                                            echo '<a href="../insert.php?id='. $row['id'] .'" class="mr-3" title="Update Record" data-toggle="tooltip"><span class="fa fa-pencil"></span></a>';
+                                            echo '<a href="../delete.php?id= '.$row['id'].' "><span class="fa fa-trash"></span></a>';
+                                            
                                         echo "</td>";
                                     echo "</tr>";
                                 }
@@ -205,6 +207,13 @@
     
 </body>
 </html>
+
+
+
+
+
+
+
 
 
 
